72186b42d5e749c487a96a16814d32b8bbdbad17
Alonso Cortes <alocortes.dev@gmail.com>
2022-07-11 17:22:23 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,86 @@
+import "./index.css";
+import { ReactComponent as ExpandIcon } from "./icons/expand-solid.svg";
+import { ReactComponent as BarsIcon } from "./icons/bars-solid.svg";
+
+import React, { useState, useEffect, useRef } from "react";
+import { CSSTransition } from "react-transition-group";
+
+function App() {
+  return (
+    <Navbar>
+      <NavItem icon={<ExpandIcon />}></NavItem>
+      <NavItem icon={<BarsIcon />}>
+        <DropdownMenu></DropdownMenu>
+      </NavItem>
+    </Navbar>
+  );
+}
+
+function Navbar(props) {
+  return (
+    <nav className="navbar">
+      <ul className="navbar-nav">{props.children}</ul>
+    </nav>
+  );
+}
+
+function NavItem(props) {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <li className="nav-item">
+      <a href="#" className="icon-button" onClick={() => setOpen(!open)}>
+        {props.icon}
+      </a>
+
+      {open && props.children}
+    </li>
+  );
+}
+
+function DropdownMenu() {
+  const [activeMenu, setActiveMenu] = useState("main");
+  const [menuHeight, setMenuHeight] = useState(null);
+  const dropdownRef = useRef(null);
+
+  useEffect(() => {
+    setMenuHeight(dropdownRef.current?.firstChild.offsetHeight);
+  }, []);
+
+  function calcHeight(el) {
+    const height = el.offsetHeight;
+    setMenuHeight(height);
+  }
+
+  function DropdownItem(props) {
+    return (
+      <a
+        href="#"
+        className="menu-item"
+        onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}
+      >
+        {props.children}
+        <span className="icon-right">{props.rightIcon}</span>
+      </a>
+    );
+  }
+
+  return (
+    <div className="dropdown" style={{ height: menuHeight }} ref={dropdownRef}>
+      <CSSTransition
+        in={activeMenu === "main"}
+        timeout={500}
+        classNames="menu-primary"
+        unmountOnExit
+        onEnter={calcHeight}
+      >
+        <div className="menu">
+          <DropdownItem>Start Over</DropdownItem>
+          <DropdownItem>Agent</DropdownItem>
+        </div>
+      </CSSTransition>
+    </div>
+  );
+}
+
+export default App;
